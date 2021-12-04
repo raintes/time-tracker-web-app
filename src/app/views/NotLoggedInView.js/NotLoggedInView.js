362f19cec397c745df417613505de7e64a49e7cb
Rai <dev.raintes@gmail.com>
2021-12-05 00:11:21 +0800

    chore(view): create not logged in state

---
+++ src/app/views/NotLoggedInView.js/NotLoggedInView.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import timeStoryset from '../../assets/images/time_storyset.png'
+
+export default function NotLoggedInView() {
+  return (
+    <>
+      <div className='ml-48 p-10 h-auto max-h-ch'>
+        <div className='flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+          <div className='my-12'>
+            <img
+              className='object-cover md:object-scale-down max-h-96 w-full'
+              alt={timeStoryset}
+              src={timeStoryset}
+            />
+          </div>
+
+          <span className='text-2xl'>
+            Time tracking for better work, not overwork
+          </span>
+          <p className='text-lg text-gray-500 mt-1'>
+            Sign in to start tracking your time now!
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/app/views/index.js
@@ -0,0 +1,3 @@
+import NotLoggedInView from './NotLoggedInView.js/NotLoggedInView'
+
+export { NotLoggedInView }
