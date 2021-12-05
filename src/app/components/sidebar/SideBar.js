1e7694b43fa36e97c880b1bc54fdbc80f0001099
Rai <dev.raintes@gmail.com>
2021-12-06 02:15:35 +0800

    chore(sidebar): deconstruct prop and set styles for selected

--- src/app/components/sidebar/SideBar.js
+++ src/app/components/sidebar/SideBar.js
@@ -9,6 +9,7 @@ function setClassNames(...classes) {
 }
 
 export default function SideBar(props) {
+  const { navigation } = props
   return (
     <div className='top-0 flex flex-col flex-grow border-r border-gray-200 pt-5 pb-4 bg-gray-800 overflow-y-auto w-48 max-h-full h-screen'>
       <div className='flex items-center flex-shrink-0 px-4 space-y-5'>
@@ -16,13 +17,13 @@ export default function SideBar(props) {
       </div>
       <div className='mt-10 flex-grow flex flex-col'>
         <nav className='flex-1  space-y-1' aria-label='Sidebar'>
-          {props.navigation.map(item => (
+          {navigation.map(item => (
             <a
               key={item.name}
               href={item.href}
               className={setClassNames(
                 item.current
-                  ? 'bg-blue-50 border-blue-600 text-blue-600'
+                  ? 'bg-gray-600 border-blue-600 text-white'
                   : 'border-transparent text-white hover:bg-gray-900 ',
                 'group flex items-center px-3 py-2 text-sm font-medium border-l-4'
               )}
