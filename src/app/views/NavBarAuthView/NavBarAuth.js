d8d6e85634afc900231609dba7d96f086ff4e640
Rai <dev.raintes@gmail.com>
2021-12-05 13:43:40 +0800

    chore(views): update styles for navbar auth and not logged in view

--- src/app/views/NavBarAuthView/NavBarAuth.js
+++ src/app/views/NavBarAuthView/NavBarAuth.js
@@ -3,26 +3,28 @@ import React from 'react'
 export default function NavBarAuthView(props) {
   return (
     <>
-      <div className='flex items-center'>
-        <div className='flex-shrink-0 space-x-5'>
-          <button
-            onClick={props.action}
-            type='button'
-            className={
-              'inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
-            }
-          >
-            <span>Login</span>
-          </button>
-          <button
-            onClick={props.action}
-            type='button'
-            className={
-              'inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
-            }
-          >
-            <span>Sign up</span>
-          </button>
+      <div className='w-screen flex items-center'>
+        <div className='w-full flex items-center justify-end'>
+          <div className='flex-shrink-0 space-x-5'>
+            <button
+              onClick={props.modal.handleShowLogin}
+              type='button'
+              className={
+                'inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
+              }
+            >
+              <span>Login</span>
+            </button>
+            <button
+              onClick={props.modal.handleShowSignup}
+              type='button'
+              className={
+                'inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
+              }
+            >
+              <span>Sign up</span>
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/app/views/NotLoggedInView/NotLoggedInView.js
+++ src/app/views/NotLoggedInView/NotLoggedInView.js
@@ -4,7 +4,7 @@ import timeStoryset from '../../assets/images/time_storyset.png'
 export default function NotLoggedInView() {
   return (
     <>
-      <div className='ml-48 p-10 h-auto max-h-ch'>
+      <div className='p-10 h-auto max-h-ch'>
         <div className='flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
           <div className='my-12'>
             <img
