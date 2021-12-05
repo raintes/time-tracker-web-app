55108cccaf5eab87378466ff5b4e71ca9d3679e0
Rai <dev.raintes@gmail.com>
2021-12-06 02:15:06 +0800

    chore(sidebar): deconstruct prop

--- src/app/components/navbar/NavBar.js
+++ src/app/components/navbar/NavBar.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
 export default function NavBar(props) {
+  const { justifyCenter, justifyEnd, justifyStart, element } = props
   return (
     <>
       <div className='static border-b border-gray-200mx-auto px-4 sm:px-6 lg:px-8 z-50 bg-white'>
-        {props.justifyEnd && (
-          <div className='flex justify-end h-16'>{props.element}</div>
+        {justifyEnd && <div className='flex justify-end h-16'>{element}</div>}
+        {justifyCenter && (
+          <div className='flex justify-center h-16'>{element}</div>
         )}
-        {props.justifyCenter && (
-          <div className='flex justify-center h-16'>{props.element}</div>
-        )}
-        {props.justifyStart && (
-          <div className='flex justify-start h-16'>{props.element}</div>
+        {justifyStart && (
+          <div className='flex justify-start h-16'>{element}</div>
         )}
       </div>
     </>
