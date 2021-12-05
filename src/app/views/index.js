a30844671fc9729fac939d3e76b8cde746c30366
Rai <dev.raintes@gmail.com>
2021-12-05 13:44:42 +0800

    chore(view): create empty state screen for time tracks

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -1,5 +1,13 @@
 import NotLoggedInView from './NotLoggedInView/NotLoggedInView'
 import TimerView from './TimerView/TimerView'
 import NavBarAuthView from './NavBarAuthView/NavBarAuth'
+import TimeWeeklyView from './TimeWeeklyView/TimeWeeklyView'
+import TimeEmptyStateView from './TimeEmptyStateView/TimeEmptyStateView'
 
-export { NotLoggedInView, TimerView, NavBarAuthView }
+export {
+  NotLoggedInView,
+  TimerView,
+  NavBarAuthView,
+  TimeWeeklyView,
+  TimeEmptyStateView,
+}
