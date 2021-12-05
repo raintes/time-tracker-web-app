946539a6493b73aa240b4b1b397cff0e13ed9d5a
Rai <dev.raintes@gmail.com>
2021-12-06 02:17:38 +0800

    chore(mock-data): create make data for projects and time entries

---
+++ src/app/mock-data/entry.list.js
@@ -0,0 +1,108 @@
+export const weekEntries = {
+  49: {
+    'November 28 2021': [
+      {
+        description: 'Create screens',
+        hours: '5:21:20',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Initial Project Setup',
+        hours: '1:30:00',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Setup CI/CD',
+        hours: '5:20:12',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+    ],
+    'November 29 2021': [
+      {
+        description: 'Create screens',
+        hours: '5:21:20',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Initial Project Setup',
+        hours: '1:30:00',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Setup CI/CD',
+        hours: '5:20:12',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+    ],
+    'November 30 2021': [
+      {
+        description: 'Create screens',
+        hours: '5:21:20',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Initial Project Setup',
+        hours: '1:30:00',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Setup CI/CD',
+        hours: '5:20:12',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+    ],
+    'December 1 2021': [
+      {
+        description: 'Create screens',
+        hours: '5:21:20',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Initial Project Setup',
+        hours: '1:30:00',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+      {
+        description: 'Setup CI/CD',
+        hours: '5:20:12',
+        modifiedOn: new Date(
+          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+        ),
+        project: 'Project 2',
+      },
+    ],
+  },
+}
---
+++ src/app/mock-data/projects.list.js
@@ -0,0 +1,6 @@
+export const projects = [
+  { id: '0', label: 'Select Project' },
+  { id: '1', label: 'Project 1' },
+  { id: '2', label: 'Project 2' },
+  { id: '3', label: 'Project 3' },
+]
