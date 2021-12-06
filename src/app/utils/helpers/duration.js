eb3fb38fc94a272021140fba3d8687fa8ba5eea2
Rai <dev.raintes@gmail.com>
2021-12-06 09:31:22 +0800

    fix(duration): optimize convert to hour code

--- src/app/utils/helpers/duration.js
+++ src/app/utils/helpers/duration.js
@@ -5,24 +5,7 @@ export const convertToSeconds = duration => {
 }
 
 export const convertSecondsToHour = duration => {
-  const hours = duration / 3600 // Find whole number by dividing the number of seconds
-  const minutes = parseFloat(hours.toString().substring(2)) * 60 // Number to the left of the decimal is the number of whole hours
-  const seconds = parseFloat(minutes.toString().substring(2)) * 60 //The number to the right of the decimal is the partial minutes.
-
-  let hh = parseInt(hours).toString()
-  let mm = parseInt(minutes).toString()
-  let ss = parseInt(seconds).toString()
-
-  // Manual adding of 0 before the number if length of number is 1
-  if (hh.length === 1) {
-    hh = '0' + hh
-  }
-  if (mm.length === 1) {
-    mm = '0' + mm
-  }
-  if (ss.length === 1) {
-    ss = '0' + ss
-  }
-
-  return hh + ':' + mm + ':' + ss
+  const measuredTime = new Date(null)
+  measuredTime.setSeconds(duration)
+  return measuredTime.toISOString().substr(11, 8)
 }
