80841f90496bbe3e856c4e9df5b0da0c1eea7cdb
Rai <dev.raintes@gmail.com>
2021-12-06 13:56:42 +0800

    chore(mock-data): update modifiedOn dates

--- src/app/mock-data/entry.list.js
+++ src/app/mock-data/entry.list.js
@@ -1,19 +1,19 @@
 export const weekEntries = {
-  49: {
+  48: {
     'December 12 2021': [
       {
         description: 'Create screens',
         hours: '5:21:20',
         modifiedOn: new Date(
-          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+          'Sun Dec 05 2021 22:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 1',
       },
       {
         description: 'Initial Project Setup',
         hours: '1:30:00',
         modifiedOn: new Date(
-          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+          'Sun Dec 05 2021 19:07:21 GMT+0800 (Philippine Standard Time)'
         ),
         project: 'Project 2',
       },
@@ -21,9 +21,9 @@ export const weekEntries = {
         description: 'Setup CI/CD',
         hours: '5:20:12',
         modifiedOn: new Date(
-          'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
+          'Sun Dec 05 2021 13:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 3',
       },
     ],
     'December 11 2021': [
@@ -33,7 +33,7 @@ export const weekEntries = {
         modifiedOn: new Date(
           'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 1',
       },
       {
         description: 'Initial Project Setup',
@@ -49,7 +49,7 @@ export const weekEntries = {
         modifiedOn: new Date(
           'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 3',
       },
     ],
     'December 10 2021': [
@@ -59,7 +59,7 @@ export const weekEntries = {
         modifiedOn: new Date(
           'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 1',
       },
       {
         description: 'Initial Project Setup',
@@ -75,7 +75,7 @@ export const weekEntries = {
         modifiedOn: new Date(
           'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 3',
       },
     ],
     'December 06 2021': [
@@ -85,7 +85,7 @@ export const weekEntries = {
         modifiedOn: new Date(
           'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 1',
       },
       {
         description: 'Initial Project Setup',
@@ -101,7 +101,7 @@ export const weekEntries = {
         modifiedOn: new Date(
           'Sun Dec 05 2021 23:07:21 GMT+0800 (Philippine Standard Time)'
         ),
-        project: 'Project 2',
+        project: 'Project 3',
       },
     ],
   },
