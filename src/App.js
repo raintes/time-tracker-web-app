6938b49ab8935ad46213ca184d9322af5069ddeb
Rai <dev.raintes@gmail.com>
2021-12-06 10:27:41 +0800

    chore(app.js): display sidebar outside of homescreen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home, Signup, Login, PageNotFound } from 'app/screens'
+import { Home, Signup, Login, PageNotFound, Report } from 'app/screens'
 import { NAVIGATION } from 'app/utils/enums/enums'
+import { navigation } from 'app/utils/constants/constants'
+import { SideBarComponent } from 'app/components'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path={NAVIGATION.HOME} element={<Home />} />
-        <Route path={NAVIGATION.SIGNUP} element={<Signup />} />
-        <Route path={NAVIGATION.LOGIN} element={<Login />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
+      <div className='justify-between flex'>
+        <div className='sidebar flex overflow-hidden min-w-sm'>
+          <SideBarComponent {...{ navigation }} />
+        </div>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path={NAVIGATION.HOME} element={<Home />} />
+          <Route path={NAVIGATION.SIGNUP} element={<Signup />} />
+          <Route path={NAVIGATION.LOGIN} element={<Login />} />
+          <Route path={NAVIGATION.REPORTS} element={<Report />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
